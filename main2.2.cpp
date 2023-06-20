2439f3525be328f6609f3910b3c8de8206161001
Luis112255 <136514022+Luis112255@users.noreply.github.com>
2023-06-20 15:07:35 -0600

    Add files via upload

---
+++ main2.2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+void invertirFrase(char* frase) {
+
+    int longitud = 0;
+    while (frase[longitud] != '\0') {
+        longitud++;
+    }
+
+
+    int inicio = 0;
+    int fin = longitud - 1;
+    while (inicio < fin) {
+       
+        char temp = frase[inicio];
+        frase[inicio] = frase[fin];
+        frase[fin] = temp;
+
+      
+        inicio++;
+        fin--;
+    }
+}
+
+int main() {
+    const int MAX_LONGITUD = 36;  
+    char frase[MAX_LONGITUD];
+
+    std::cout << "Ingrese una frase (max. 35 caracteres): ";
+    std::cin.getline(frase, MAX_LONGITUD);
+
+  
+    invertirFrase(frase);
+
+    std::cout << "Frase invertida: " << frase << std::endl;
+
+    return 0;
+}
